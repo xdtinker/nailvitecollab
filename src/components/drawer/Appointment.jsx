6fa17f4803e703801284a79e54a39a75fe63e57b
Aziz <aziz.saricula@gmail.com>
2023-12-06 15:01:39 +0800

    fixed minor bugs

--- src/components/drawer/Appointment.jsx
+++ src/components/drawer/Appointment.jsx
@@ -7,12 +7,6 @@ import {
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../config/firestore.js";
 
-const statuses = {
-  Paid: "text-green-700 bg-green-50 ring-green-600/20",
-  Withdraw: "text-gray-600 bg-gray-50 ring-gray-500/10",
-  Overdue: "text-red-700 bg-red-50 ring-red-600/10",
-};
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
